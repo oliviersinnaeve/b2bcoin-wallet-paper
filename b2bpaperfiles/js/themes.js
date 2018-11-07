661eae4f2d9668a5c76d167d023e0c8ac1692888
PorkyForky <35432318+PorkyForky@users.noreply.github.com>
2018-11-07 23:28:50 +0100

    Put back some code accidently removed :P

--- b2bpaperfiles/js/themes.js
+++ b2bpaperfiles/js/themes.js
@@ -1,3 +1,17 @@
+$(document).ready(function () {
+  $('textarea[data-limit-rows=true]')
+    .on('keypress', function (event) {
+        var textarea = $(this),
+            text = textarea.val(),
+            numberOfLines = (text.match(/\n/g) || []).length + 1,
+            maxRows = parseInt(textarea.attr('rows'));
+
+        if (event.which === 13 && numberOfLines === maxRows ) {
+          return false;
+        }
+    });
+});
+
 function getTheme(selectObject) {
   var selectedTheme = selectObject.value;
 
